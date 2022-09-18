85571ea738108d5ce7c992f5ca11f9a223412c09
venkat <subash.venkat007@gmail.com>
2022-09-18 11:10:07 +0530

    added pico css styling

--- pages/admin/AddProducts.vue
+++ pages/admin/AddProducts.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
-
+const productName: Ref<null | string> = ref(null);
+const addProduct = () => {};
 </script>
 
 <template>
-    <form @>
-
-    </form>
+  <form @submit.prevent="addProduct">
+    <!-- <label for="productImage">Product Image</label>
+        <input type="file" name="productImage" id="productImage" @change="image"> -->
+    <fieldset>
+      <label for="productName">Product name </label>
+      <input type="text" id="productName" v-model="productName" />
+    </fieldset>
+    <fieldset>
+      <label for="productPrice">Product Price</label>
+      <input type="text" />
+    </fieldset>
+    <button type="submit" class="contrast">
+      <u>
+        <b> Add Product </b>
+      </u>
+    </button>
+  </form>
+  {{ productName }}
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
